533aca7bca0022e84d5f9f2c125fb0b0b6751807
amirhosseinbagheri <ahbagheri01@gmail.com>
2021-10-10 18:00:35 +0330

    scanner init added structure are unknown

--- Scanner.py
+++ Scanner.py
@@ -1,3 +1,4 @@
+
 class Scanner:
     def __init__(self,path):
         input_file = open(path)
@@ -5,6 +6,18 @@ class Scanner:
         self.line_number = 1
         self.char_index = 0
         self.input_size = len(self.input)
+        self.DFA =
         while self.char_index < self.input_size :
             print(self.input[self.char_index])
             self.char_index= self.char_index+1
+    def get_next_token(self):
+        pass #TODO
+class DFA:
+    def __init__(self):
+        pass #TODO
+class Symbol_Table:
+    pass # TODO
+class Token_Table:
+    pass # TODO
+class Error_Table:
+    pass # TODO
