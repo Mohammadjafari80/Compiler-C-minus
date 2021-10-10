ef19d3bbf6bc6bee147104d9dda788ff23906539
amirhosseinbagheri <ahbagheri01@gmail.com>
2021-10-10 18:01:15 +0330

    scanner init added structure are unknown

--- Scanner.py
+++ Scanner.py
@@ -6,7 +6,7 @@ class Scanner:
         self.line_number = 1
         self.char_index = 0
         self.input_size = len(self.input)
-        self.DFA =
+        #self.DFA =
         while self.char_index < self.input_size :
             print(self.input[self.char_index])
             self.char_index= self.char_index+1
