f72de0c1642c8f37f39e5645c57a2d030b7e7ea5
amirhosseinbagheri <ahbagheri01@gmail.com>
2021-10-14 13:10:18 +0330

    added get token

---
+++ DFA/DFA.py
@@ -0,0 +1,169 @@
+class DFA:
+    def __init__(self, load_mode=False, save_mode=False, load_path="dfa.txt", save_path="dfa.txt"):
+        """
+        initialize the DFA module
+        """
+
+        self.states = {-1: Node(node_name=-1, is_it_final=True, is_it_error=True, error_type=Error.invalid_input),
+                        0: Node(node_name=0),
+                        1: Node(node_name=1),
+                        2: Node(node_name=2, is_it_final=True, is_it_error=True, error_type=Error.invalid_number),
+                        3: Node(node_name=3, is_it_final=True,lookahead=True,token_type=Token.num,token_matter=True),
+                        4: Node(node_name=4),
+                        5: Node(node_name=5, is_it_final=True,lookahead=True,token_type=Token.id,token_matter=True),
+                        6: Node(node_name=6, is_it_final=True,token_type=Token.symbol,token_matter=True),
+                        7: Node(node_name=7),
+                        8: Node(node_name=8, is_it_final=True,lookahead=True,token_type=Token.symbol,token_matter=True),
+                        9: Node(node_name=9, is_it_final=True,token_type=Token.symbol,token_matter=True),
+                       10: Node(node_name=10),
+                       11: Node(node_name=11, is_it_final=True,lookahead=True,token_type=Token.white_space),
+                       12: Node(node_name=12),
+                       13: Node(node_name=13, is_it_final=True, is_it_error=True, error_type=Error.unmatched_comment),
+                       14: Node(node_name=14, is_it_final=True,lookahead=True,token_type=Token.symbol,token_matter=True),
+                       15: Node(node_name=15),
+                       16: Node(node_name=16),
+                       17: Node(node_name=17,is_it_final=True,token_type=Token.comment),
+                       18: Node(node_name=18),
+                       19: Node(node_name=19),
+                    }
+
+        try:
+            if load_mode :
+                self.table = self.load_table(load_path)
+            else:
+                self.table = self.initialize_table()
+            if save_mode:
+                self.save_dfa(save_path)
+        except:
+            pass
+    def get_state(self,current_state:int,c:int) -> Node:
+        return self.states.get(self.table[current_state][c])
+    def initialize_table(self)-> list:
+        """[summary]
+            Returns:
+                list: 2D list of all transitions and state
+            """
+        table = [[-1 for i in range(256)] for j in range(20)]
+        """ initialization of table """
+        for i in range(256):
+            if self.is_is_digits(i):
+                table[0][i] = 1
+                table[1][i] = 1
+                table[4][i] = 4
+            if self.is_it_letter(i):
+                table[0][i] = 4
+                table[4][i] = 4
+                table[1][i] = 2
+            if self.is_it_white_space(i):
+                table[0][i] = 10
+                table[10][i] = 10
+            else:
+                table[10][i] = 11
+            if self.is_it_unique_symbol(i):
+                table[0][i] = 6
+            elif i == 61:  # is input is =
+                table[0][i] = 7
+                table[7][i] = 9
+            else:
+                table[7][i] = 8  # remark the others of symbol table
+            if i == 47:  # if input is /
+                table[0][i] = 15
+                table[12][i] = 13
+                table[15][i] = 16
+                table[19][i] = 17
+            else:
+                table[12][i] = 14
+            if i == 42:  # is input is *
+                table[0][i] = 12
+                table[15][i] = 18
+                table[18][i] = 19
+                table[19][i]= 19
+            else:
+                table[18][i] = 18  # remark the others of comment for state 12
+                if i != 47:
+                    table[19][i] = 18  # remark others of comment for state 13
+            if self.is_it_IDorNum_others(i):
+                table[1][i] = 3
+                table[4][i] = 5
+            if i == 10:
+                table[16][i] = 17
+            else:
+                table[16][i] = 16
+        return table
+
+    def is_it_IDorNum_others(self, c: int) -> bool:
+        """
+
+        :param c:
+        :return:
+        """
+        return self.is_it_white_space(c) or self.is_it_symbol(c) or c == 47
+
+    def is_it_unique_symbol(self, c: int) -> bool:
+        """
+        :return:
+        """
+        return (c != 42) and ((40 <= c <= 45) or (58 <= c <= 60) or (c == 91) or c == 93 or c == 123 or c == 126)
+
+    def is_it_symbol(self, c: int) -> bool:
+        """
+        :return:
+        """
+        return (40 <= c <= 45) or (58 <= c <= 61) or (c == 91) or c == 93 or c == 123 or c == 126
+
+    def is_it_white_space(self, c: int) -> bool:
+        """
+
+        :param c:
+        :return: return whether c is a white space or not
+        """
+        return (9 <= c <= 13) or (c == 32)
+
+    def is_is_digits(self, c: int) -> bool:
+        """
+
+        :param c: get the ord of the input char
+        :return: return whether the char is digit or not [0-9]
+        """
+        return c >= 48 and c <= 57
+
+    def is_it_letter(self, c: int) -> bool:
+        """
+
+        :param c: char input as an ord
+        :return: return whether it is [a-z,A-Z] or not
+        """
+        return (97 <= c <= 122) or (65 <= c <= 90)
+
+    def save_dfa(self, path: str):
+        """
+        this function save the dfa in file txt
+        :param path: get path to save file as txt
+        :return:
+        """
+        wr = ""
+        for i in range(len(self.table)):
+            for j in range(len(self.table[0])):
+                wr += str(self.table[i][j]) + " "
+            wr += "\n"
+        f = open(path, "w")
+        f.write(wr)
+        f.close()
+
+    def load_table(self,path:str)->list:
+        """
+
+        :param path: get input txt file path
+        :return: return the 2D array of DFAS
+        """
+        self.table = []
+        with open(path, "r") as f:
+            for line in f:
+                line.replace('\n', ' ')
+                temp = list(map(int,line.split()))
+                self.table.append(temp)
+        f.close()
+        return self.table
+
+
+
--- Scanner.py
+++ Scanner.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+import DFA.DFA
 
 class Token(Enum):
     id = "ID"
@@ -41,11 +41,9 @@ class Scanner:
         self.buffer = ""
         self.buffer_size = 0
         self.DFA = DFA(load_mode=False, save_mode=True, save_path="dfa.txt")
+        self.symbol_Table = SymbolTable()
 
     def get_next_token(self):
-
-
-
         while self.char_index < self.buffer_size:
             input_char = self.buffer[self.char_index]
             ascii_code = ord(input_char)
@@ -70,25 +68,27 @@ class Scanner:
             self.char_index += 1
             if next_state.lookahead:
                 self.char_index -= 1
-
-
-
-
+    def get_token_string(self,state,lexeme):
+        if state.token_type == Token.id:
+            (id,type_of_token,lexeme) = self.symbol_Table.get_token(lexeme)
+            return "("+str(type_of_token.values)+", "+str(lexeme)+")"
+        else:
+            return "("+str(state.token_type.values)+", "+str(lexeme)+")"
 class SymbolTable:
 
     def __init__(self):
         self.table = {"if":1, "else":2, "void": 3, "int":4, "repeat":5, "break":6, "until":7, "return":8}
         self.last_id = 9
 
-    def get_token(self, ID:str) -> (int,Token,str):
-        temp = self.table.get(ID,None)
+    def get_token(self, lexeme:str) -> (int,Token,str):
+        temp = self.table.get(lexeme,None)
         if temp is None:
-            self.table[ID] = self.last_id
+            self.table[lexeme] = self.last_id
             self.last_id+=1
-            return self.last_id, Token.id, ID
+            return self.last_id, Token.id, lexeme
         if temp <= 8 :
-            return temp, Token.keyword, ID
-        return temp, Token.id, ID
+            return temp, Token.keyword, lexeme
+        return temp, Token.id, lexeme
 
     pass
 
@@ -112,175 +112,6 @@ class Node:
         self.lookahead = lookahead
         self.token_type = token_type
         self.token_matter = token_matter
-
-
-
-class DFA:
-    def __init__(self, load_mode=False, save_mode=False, load_path="dfa.txt", save_path="dfa.txt"):
-        """
-        initialize the DFA module
-        """
-
-        self.states = {-1: Node(node_name=-1, is_it_final=True, is_it_error=True, error_type=Error.invalid_input),
-                        0: Node(node_name=0),
-                        1: Node(node_name=1),
-                        2: Node(node_name=2, is_it_final=True, is_it_error=True, error_type=Error.invalid_number),
-                        3: Node(node_name=3, is_it_final=True,lookahead=True,token_type=Token.num,token_matter=True),
-                        4: Node(node_name=4),
-                        5: Node(node_name=5, is_it_final=True,lookahead=True,token_type=Token.id,token_matter=True),
-                        6: Node(node_name=6, is_it_final=True,token_type=Token.symbol,token_matter=True),
-                        7: Node(node_name=7),
-                        8: Node(node_name=8, is_it_final=True,lookahead=True,token_type=Token.symbol,token_matter=True),
-                        9: Node(node_name=9, is_it_final=True,token_type=Token.symbol,token_matter=True),
-                       10: Node(node_name=10),
-                       11: Node(node_name=11, is_it_final=True,lookahead=True,token_type=Token.white_space),
-                       12: Node(node_name=12),
-                       13: Node(node_name=13, is_it_final=True, is_it_error=True, error_type=Error.unmatched_comment),
-                       14: Node(node_name=14, is_it_final=True,lookahead=True,token_type=Token.symbol,token_matter=True),
-                       15: Node(node_name=15),
-                       16: Node(node_name=16),
-                       17: Node(node_name=17,is_it_final=True,token_type=Token.comment),
-                       18: Node(node_name=18),
-                       19: Node(node_name=19),
-                    }
-
-        try:
-            if load_mode :
-                self.table = self.load_table(load_path)
-            else:
-                self.table = self.initialize_table()
-            if save_mode:
-                self.save_dfa(save_path)
-        except:
-            pass
-    def get_state(self,current_state:int,c:int) -> Node:
-        return self.state.get(self.table[current_state][c])
-    def initialize_table(self)-> list:
-        """[summary]
-            Returns:
-                list: 2D list of all transitions and state
-            """
-        table = [[-1 for i in range(256)] for j in range(20)]
-        """ initialization of table """
-        for i in range(256):
-            if self.is_is_digits(i):
-                table[0][i] = 1
-                table[1][i] = 1
-                table[4][i] = 4
-            if self.is_it_letter(i):
-                table[0][i] = 4
-                table[4][i] = 4
-                table[1][i] = 2
-            if self.is_it_white_space(i):
-                table[0][i] = 10
-                table[10][i] = 10
-            else:
-                table[10][i] = 11
-            if self.is_it_unique_symbol(i):
-                table[0][i] = 6
-            elif i == 61:  # is input is =
-                table[0][i] = 7
-                table[7][i] = 9
-            else:
-                table[7][i] = 8  # remark the others of symbol table
-            if i == 47:  # if input is /
-                table[0][i] = 15
-                table[12][i] = 13
-                table[15][i] = 16
-                table[19][i] = 17
-            else:
-                table[12][i] = 14
-            if i == 42:  # is input is *
-                table[0][i] = 12
-                table[15][i] = 18
-                table[18][i] = 19
-                table[19][i]= 19
-            else:
-                table[18][i] = 18  # remark the others of comment for state 12
-                if i != 47:
-                    table[19][i] = 18  # remark others of comment for state 13
-            if self.is_it_IDorNum_others(i):
-                table[1][i] = 3
-                table[4][i] = 5
-            if i == 10:
-                table[16][i] = 17
-            else:
-                table[16][i] = 16
-        return table
-
-    def is_it_IDorNum_others(self, c: int) -> bool:
-        """
-
-        :param c:
-        :return:
-        """
-        return self.is_it_white_space(c) or self.is_it_symbol(c) or c == 47
-
-    def is_it_unique_symbol(self, c: int) -> bool:
-        """
-        :return:
-        """
-        return (c != 42) and ((40 <= c <= 45) or (58 <= c <= 60) or (c == 91) or c == 93 or c == 123 or c == 126)
-
-    def is_it_symbol(self, c: int) -> bool:
-        """
-        :return:
-        """
-        return (40 <= c <= 45) or (58 <= c <= 61) or (c == 91) or c == 93 or c == 123 or c == 126
-
-    def is_it_white_space(self, c: int) -> bool:
-        """
-
-        :param c:
-        :return: return whether c is a white space or not
-        """
-        return (9 <= c <= 13) or (c == 32)
-
-    def is_is_digits(self, c: int) -> bool:
-        """
-
-        :param c: get the ord of the input char
-        :return: return whether the char is digit or not [0-9]
-        """
-        return c >= 48 and c <= 57
-
-    def is_it_letter(self, c: int) -> bool:
-        """
-
-        :param c: char input as an ord
-        :return: return whether it is [a-z,A-Z] or not
-        """
-        return (97 <= c <= 122) or (65 <= c <= 90)
-
-    def save_dfa(self, path: str):
-        """
-        this function save the dfa in file txt
-        :param path: get path to save file as txt
-        :return:
-        """
-        wr = ""
-        for i in range(len(self.table)):
-            for j in range(len(self.table[0])):
-                wr += str(self.table[i][j]) + " "
-            wr += "\n"
-        f = open(path, "w")
-        f.write(wr)
-        f.close()
-
-    def load_table(self,path:str)->list:
-        """
-
-        :param path: get input txt file path
-        :return: return the 2D array of DFAS
-        """
-        self.table = []
-        with open(path, "r") as f:
-            for line in f:
-                line.replace('\n', ' ')
-                temp = list(map(int,line.split()))
-                self.table.append(temp)
-        f.close()
-        return self.table
 
 
 
