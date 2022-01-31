7e03400923ba61045694c52712d88e24e469c5e7
amirhoosein <ahbagheri01@gmail.com>
2022-01-31 15:18:07 +0330

    final

--- code_gen/Memory_handler.py
+++ code_gen/Memory_handler.py
@@ -4,8 +4,10 @@ from scope_records import scope_record as sr
 class Memory():
     def __init__(self):
         self.code_add = 0
+        self.sp = 1000
         self.static_data = 100
         self.dynamic_data = 500
+        # self.dynamic_stack = 1000
 
     def get_temp(self):
         self.dynamic_data += 4
